ddf654b79ee1605ba2ad1073f15c50acdcb1335f
Viraj Mehta <virajm@cs.cmu.edu>
2022-07-27 00:03:21 -0400

    added script file

---
+++ run_all.sh
@@ -0,0 +1,5 @@
+# python scripts/train.py --config_name="beta_tracking" --strategy="information" --seed=0 --logdir="beta_tracking"
+# python scripts/train.py --config_name="beta_rotation" --strategy="information" --seed=0 --logdir="beta_rotation"
+# python scripts/train.py --config_name="cartpole" --strategy="information" --seed=0 --logdir="cartpole"
+# python scripts/train.py --config_name="pendulum" --strategy="information" --seed=0 --logdir="pendulum"
+python scripts/train.py --config_name="barl_reacher" --strategy="information" --seed=0 --logdir="reacher"
